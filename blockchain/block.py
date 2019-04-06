ea4800c5b9dae9610896612ecd33c888e67ba667
Per Høgfeldt <hogfeldt38@gmail.com>
2019-04-06 22:51:37 +0200

    Pushing code as it is now, so others can read it. Not finished

--- blockchain/block.py
+++ blockchain/block.py
@@ -1,6 +1,32 @@
 import time
 
-from utils import calculate_hash
+BLOCK_GENERATION_INTERVAL = 10
+
+DIFFICULTY_ADJUSTMENT_INTERVAL = 10
+
+
+def get_genesis_block():
+    return GENESIS_BLOCK
+
+
+def get_block_generation_interval():
+    return BLOCK_GENERATION_INTERVAL
+
+
+def get_difficulty_adjustment_interval():
+    return DIFFICULTY_ADJUSTMENT_INTERVAL
+
+
+def calculate_hash(index, previous_hash, timestamp, data):
+    return str(
+        hashlib.sha256(
+            str(index).encode()
+            + previous_hash.encode()
+            + str(timestamp).encode()
+            + data
+        ).digest()
+    )
+
 
 class Block:
     def __init__(self, index, hash, previous_hash, timestamp, data, difficulty, nonce):
@@ -16,15 +42,39 @@ class Block:
         index = self.index + 1
         previous_hash = self.hash
         timestamp = time.time()
-        hash = calculate_hash(index, previous_hash, timestamp, data)
+        hash = calculate_hash(index, previous_hash, timestamp, data.encode()).decode()
         return Block(index, hash, previous_hash, timestamp, data)
 
     def __str__(self):
-        return "{index: %s, hash: %s, previous_hash: %s, timestamp: %s, data: %s}" % (
+        return "{index: %s, hash: %s, previous_hash: %s, timestamp: %s, data: %s, difficulty: %s, nonce: %s}" % (
             self.index,
             self.hash,
             self.previous_hash,
             self.timestamp,
             self.data,
+            self.difficulty,
+            self.nonce,
         )
 
+
+def dict_to_block(dict_block):
+    return Block(
+        int(dict_block["index"]),
+        dict_block["hash"],
+        dict_block["previous_hash"],
+        float(dict_block["timestamp"]),
+        dict_block["data"],
+        int(dict_block["difficulty"]),
+        int(dict_block["nonce"]),
+    )
+
+
+GENESIS_BLOCK = Block(
+    0,
+    "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7",
+    None,
+    1554544077.638083,
+    "My genises!!",
+    4,
+    0,
+)
--- client/client.py
+++ client/client.py
@@ -1,9 +1,10 @@
 import socket
 import time
+import json
 from socket import error as SocketError
 import _thread as thread
 
-from blockchain.block import Block
+from blockchain.block import Block, dict_to_block
 
 
 target = "127.0.0.1"
@@ -22,20 +23,42 @@ def listener():
 
     while True:
         try:
-            clientsocket, adress = server_socket.accept()
+            connection_socket, adress = server_socket.accept()
             while "\n" not in message_buffer:
-                raw_buff = clientsocket.recv(1024)
+                raw_buff = connection_socket.recv(1024)
                 message_buffer += raw_buff.decode()
             request = message_buffer.split("\n")[0]
             message_buffer = message_buffer.split("\n")[1:]
 
             # Handle request
             if "SIGNAL chain changed" in request:
+                while "\n" not in message_buffer:
+                    raw_buff = connection_socket.recv(1024)
+                    message_buffer += raw_buff.decode()
+                chain_length = message_buffer.split("\n")[0]
+                while len([b for b in message_buffer.split("\n") if b]) < chain_length:
+                    raw_buff = connection_socket.recv(1024)
+                    message_buffer += raw_buff.decode()
+                json_blocks = message_buffer.split("\n")[:chain_length]
+                new_chain = [dict_to_block(json.loads(j)) for j in json_blocks]
+                
                 # ask for the new chain
                 pass
             elif "GET chain" in request:
-                # send back a copy of chain
+                chain_length = "%s\n" % len(chain)
+                connection_socket.sendall(chain_length.encode())
+                for b in chain:
+                    block_to_send = "%s\n" % json.dumps(b.__dict__)
+                    connection_socket.sendall(block_to_send.encode())
+            elif "SIGNAL peers changed" in request:
+                # ash for new peer list
                 pass
+            elif "GET peers" in request:
+                peers_length = "%s\n" % len(peers)
+                connection_socket.sendall(peers_length.encode())
+                for p in peers:
+                    peer_to_send = "%s\n" % p
+                    connection_socket.sendall(peer_to_send.encode())
 
         except SocketError as e:
             if e.errno != errno.ECONNRESET:
@@ -54,6 +77,7 @@ def worker():
             try:
                 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 client_socket.connect((target, 9000))
+                # mine chain
                 client_socket.sendall("F".encode())
                 time.sleep(1)
             except ConnectionRefusedError as e:
@@ -69,17 +93,32 @@ def get_start_info_from_server():
     """ Ask the content provider for the chain and peers """
     content_provider = "0.0.0.0"
     message_buffer = ""
+    peers = []
+    chain = []
     try:
         # Socket setup
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client_socket.connect((content_provider, 9000))
         # Reauest chain
-        start_request = "GET chain \n"
+        start_request = "GET chain\n"
         client_socket.sendall(start_request.encode())
-        
+        while "\n" not in message_buffer:
+            raw_data = client_socket.recv(1024)
+            message_buffer += raw_buff.decode()
+        # TODO: create a constructor for Block taking a json string
+        client_socket.close()
 
-        # Request peers
-        return ([], [])
+        # Socket setup
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((content_provider, 9000))
+        # Reauest peers
+        start_request = "GET peers\n"
+        client_socket.sendall(start_request.encode())
+        while "\n" not in message_buffer:
+            raw_data = client_socket.recv(1024)
+            message_buffer += raw_buff.decode()
+        # TODO: create a peers list taking a json string
+        return (chain, peers)
     finally:
         client_socket.close()
 
